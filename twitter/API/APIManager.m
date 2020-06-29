27de5bc7eabbfccf8706d4d0d1a2c6c05c865310
Ogo-Oluwasubomi Popoola <subomi@fb.com>
2020-06-29 15:35:34 -0400

    Starter Twitter Template

---
+++ twitter/API/APIManager.m
@@ -0,0 +1,64 @@
+//
+//  APIManager.m
+//  twitter
+//
+//  Created by emersonmalca on 5/28/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import "APIManager.h"
+#import "Tweet.h"
+
+static NSString * const baseURLString = @"https://api.twitter.com";
+static NSString * const consumerKey = @"uFTmFW66AAMEUwx3rZlZDMSCf";
+static NSString * const consumerSecret = @"LtlxIoQpBvHcqjpSMIA9Gs2E9wCJbr7xkx9EpSdBYoNedaZUgh";
+
+@interface APIManager()
+
+@end
+
+@implementation APIManager
+
++ (instancetype)shared {
+    static APIManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+- (instancetype)init {
+    
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSString *key = consumerKey;
+    NSString *secret = consumerSecret;
+    // Check for launch arguments override
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"]) {
+        key = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"];
+    }
+    if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-secret"]) {
+        secret = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-secret"];
+    }
+    
+    self = [super initWithBaseURL:baseURL consumerKey:key consumerSecret:secret];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)getHomeTimelineWithCompletion:(void(^)(NSArray *tweets, NSError *error))completion {
+    
+    [self GET:@"1.1/statuses/home_timeline.json"
+    parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSArray *  _Nullable tweetDictionaries) {
+        // Success
+        NSMutableArray *tweets  = [Tweet tweetsWithArray:tweetDictionaries];
+        completion(tweets, nil);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        // There was a problem
+        completion(nil, error);
+    }];
+}
+
+@end
---
+++ twitter/Models/Tweet.m
@@ -0,0 +1,54 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by Ogo-Oluwasobomi Popoola on 6/29/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+#import "User.h"
+
+@implementation Tweet
+
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries{
+    NSMutableArray *tweets = [NSMutableArray array];
+    for (NSDictionary *dictionary in dictionaries) {
+        Tweet *tweet = [[Tweet alloc] initWithDictionary:dictionary];
+        [tweets addObject:tweet];
+    }
+    return tweets;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        NSDictionary *originalTweet = dictionary[@"retweeted_status"];
+        if(originalTweet != nil){
+            NSDictionary *userDictionary = dictionary[@"user"];
+            self.retweetedByUser = [[User alloc] initWithDictionary:userDictionary];
+            dictionary = originalTweet;
+        }
+        self.idStr = dictionary[@"id_str"];
+        self.text = dictionary[@"text"];
+        self.favoriteCount = [dictionary[@"favorite_count"] intValue];
+        self.favorited = [dictionary[@"favorited"] boolValue];
+        self.retweetCount = [dictionary[@"retweet_count"] intValue];
+        self.retweeted = [dictionary[@"retweeted"] boolValue];
+        
+        //Getting user Info
+        NSDictionary *user = dictionary[@"user"];
+        self.user = [[User alloc] initWithDictionary:user];
+        NSString *createdAtOriginalString = dictionary[@"created_at"];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+        NSDate *date = [formatter dateFromString:createdAtOriginalString];
+        formatter.dateStyle = NSDateFormatterShortStyle;
+        formatter.timeStyle = NSDateFormatterNoStyle;
+        self.createdAtString = [formatter stringFromDate:date];
+        
+    }
+    return self;
+}
+
+@end
